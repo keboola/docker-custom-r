bb0c36c8ff954095544a10e7a922b9b2a5fa87e4
Odin <odinuv@gmail.com>
2015-11-17 09:47:00 +0100

    feat: add caret package

--- init.R
+++ init.R
@@ -2,7 +2,7 @@
 install.packages(c('git2r', 'jsonlite', 'devtools', 'rJava', 'RJDBC'), repos = 'http://cran.us.r-project.org', dependencies = c("Depends", "Imports", "LinkingTo"), INSTALL_opts = c("--no-html"))
 
 # install some commonly used packages
-install.packages(c('stringr', 'corrgram', 'data.table', 'gbm', 'ggplot2', 'leaps', 'plyr', 'dplyr', 'ggvis', 'moments', 'Rcpp', 'data.table', 'quantreg', 'httr'), repos = 'http://cran.us.r-project.org', dependencies = c("Depends", "Imports", "LinkingTo"), INSTALL_opts = c("--no-html"))
+install.packages(c('stringr', 'corrgram', 'data.table', 'gbm', 'ggplot2', 'leaps', 'plyr', 'dplyr', 'ggvis', 'moments', 'Rcpp', 'data.table', 'quantreg', 'httr', 'caret'), repos = 'http://cran.us.r-project.org', dependencies = c("Depends", "Imports", "LinkingTo"), INSTALL_opts = c("--no-html"))
 
 library('devtools')
 
