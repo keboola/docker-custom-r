ffe91b708d8ef5e06f0aa54e3638a991ed4f78b4
Odin <odinuv@gmail.com>
2016-02-24 10:19:43 +0100

    fix: update library to handle KBC_DATADIR env variable

--- init.R
+++ init.R
@@ -2,4 +2,4 @@ library('devtools')
 
 # install the R application
 install_github('keboola/r-application', ref = "master")
-install_github('keboola/r-docker-application', ref = "master")
+install_github('keboola/r-docker-application', ref = "0.0.1")
