ca88595e5ebe1a4f0ec31c126153cad03d137bfe
Ondrej Popelka <odinuv@gmail.com>
2017-10-04 16:41:01 +0200

    fix: install packages with warning check where possible fix: install nloptr through install.packages

--- Dockerfile
+++ Dockerfile
@@ -11,31 +11,26 @@ RUN mkdir /usr/share/doc/R${R_VERSION} \
 
 # Install dependencies for packages (gdal)
 RUN apt-get update \
-	&& apt-get install -y --no-install-recommends \
+    && apt-get install -y --no-install-recommends \
         ca-certificates \
         curl \
         default-jdk \
-        ed \        
-		libgdal-dev \
-		libproj-dev \
-        wget \		
+        ed \
+        libbz2-dev \
+        libcairo2-dev \
+        libgdal-dev \
+        libgsl0-dev \
+        libproj-dev \
+        libssl-dev \
+        libx11-dev \
+        libxt-dev \
+        xfonts-base \
+        unzip \
+        wget \
+        x11proto-core-dev \
     && rm -rf /var/lib/apt/lists/*
-
-# Install nloptr package
-RUN curl -f -L http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz > /tmp/nlopt-2.4.2.tar.gz \
-	&& curl -f -L https://cran.r-project.org/src/contrib/nloptr_1.0.4.tar.gz > /tmp/nloptr_1.0.4.tar.gz \
-	&& tar xvfz /tmp/nloptr_1.0.4.tar.gz \
-	&& head /tmp/nloptr/configure -n 3300 > /tmp/nloptr/configure.new \
-	&& echo "   ## Keboola fix to build locally" >> /tmp/nloptr/configure.new \
-	&& echo "   \$(/usr/local/lib/R/bin/Rscript --vanilla -e \"file.copy(from='/tmp/nlopt-2.4.2.tar.gz', to='\${NLOPT_TGZ}')\")" >> /tmp/nloptr/configure.new \
-	&& tail -n +3302 /tmp/nloptr/configure >> /tmp/nloptr/configure.new \
-	&& rm -rf /tmp/nloptr/configure \
-	&& mv /tmp/nloptr/configure.new /tmp/nloptr/configure \
-	&& chmod u+x /tmp/nloptr/configure \
-	&& R CMD INSTALL nloptr \
-	&& rm -rf /tmp/nlopt-2.4.2.tar.gz /tmp/nloptr_1.0.4.tar.gz /tmp/nloptr
 
 COPY init.R /tmp/init.R
 
-# Install some commonly used R packages 
+# Install some commonly used R packages
 RUN R CMD javareconf && /usr/local/lib/R/bin/Rscript /tmp/init.R
--- init.R
+++ init.R
@@ -1,44 +1,51 @@
 # install really required packages
-install.packages(
-	c('Cairo', 'curl', 'DBI', 'devtools', 'git2r', 'jsonlite', 'rJava', 'RJDBC', 'roxygen2', 'rversions', 'rstudioapi'), 
-	dependencies = c("Depends", "Imports", "LinkingTo"), 
-	INSTALL_opts = c("--no-html")
-)
 
-# install some commonly used packages
+withCallingHandlers(install.packages(
+    c('Cairo', 'codetools', 'curl', 'DBI', 'devtools', 'git2r', 'jsonlite', 'rJava', 'RJDBC', 'roxygen2', 'rversions', 'rstudioapi'), 
+    dependencies = c("Depends", "Imports", "LinkingTo"), 
+    INSTALL_opts = c("--no-html")
+), warning = function(w) stop(w))
+
+#install some commonly used packages
+withCallingHandlers(install.packages(
+    c(
+        'ascii', 
+        'BH', 'bitops', 'brew', 'BTYD',
+        'car', 'caret', 'caTools', 'corrgram', 'Cubist',
+        'data.table', 'data.tree', 'digest', 'doParallel', 'dplyr',
+        'evaluate', 'earth', 'ellipse', 'e1071',
+        'forecast', 'foreach',
+        'gam', 'gbm', 'gdata', 'ggplot2', 'ggvis', 'gsl',
+        'httr',
+        'ipred', 'ISOweek',
+        'kernlab', 'klaR', 
+        'lattice', 'leaps',
+        'MASS', 'magrittr', 'mda', 'mime', 'moments', 'memoise', 'mgcv', 'mlbench',
+        'nlme', 'nloptr', 'nnet', 
+        'outliers', 
+        'party', 'pamr', 'pls', 'plyr', 'pROC', 'proxy',
+        'qdap', 'quantreg',
+        'randomForest', 'RANN', 'reshape2', 'R6', 'Rcpp', 'RcppArmadillo', 'rgdal',
+        'sp', 'spls', 'sqldf', 'stringi', 'stringr', 'subselect', 'superpc',
+        'testthat', 'timeDate', 'tree',
+        'whisker',
+        'xml2',
+        'zoo'
+    ), 
+    dependencies = c("Depends", "Imports", "LinkingTo"), 
+    INSTALL_opts = c("--no-html")
+), warning = function(w) stop(w))
+
+#install packages which throw warnings (unsupported version)
 install.packages(
-	c(
-		'ascii', 
-		'BH', 'bitops', 'brew', 'BTYD',
-		'car', 'caret', 'caTools', 'corrgram', 'Cubist',
-		'datasets', 'data.table', 'data.tree', 'digest', 'ddd', 'doParallel', 'dplyr',
-		'evaluate', 'earth', 'ellipse', 'e1071',
-		'forecast', 'foreach',
-		'gam', 'gbm', 'gdata', 'ggplot2', 'ggvis', 'graphics', 'grDevices', 'gsl',
-		'httr',
-		'ipred', 'ISOweek',
-		'kernlab', 'klaR', 
-		'lattice', 'leaps',
-		'MASS', 'magrittr', 'mda', 'mime', 'moments', 'memoise', 'methods', 'mgcv', 'mlbench',
-		'nlme', 'nnet', 
-		'outliers', 
-		'party', 'pamr', 'pls', 'plyr', 'pROC', 'proxy',
-		'qdap', 'quantreg',
-		'randomForest', 'RANN', 'reshape2', 'R6', 'Rcpp', 'RcppArmadillo', 'rgdal',
-		'sp', 'spls', 'sqldf', 'stringi', 'stringr', 'subselect', 'superpc', 'stats',
-		'testthat', 'timeDate', 'tree',
-    	'utils',
-    	'whisker',
-    	'xml2',
-    	'zoo'
-	), 
-	dependencies = c("Depends", "Imports", "LinkingTo"), 
-	INSTALL_opts = c("--no-html")
+    c('datasets', 'ddd', 'graphics', 'grDevices', 'methods', 'stats', 'utils'), 
+    dependencies = c("Depends", "Imports", "LinkingTo"), 
+    INSTALL_opts = c("--no-html")
 )
 
 packageurl <- "https://cran.r-project.org/src/contrib/Archive/ChannelAttribution/ChannelAttribution_1.7.tar.gz"
 install.packages(packageurl, repos=NULL, type="source")
 
 # install the R application
-install_github('keboola/r-application', ref = "master")
-install_github('keboola/r-docker-application', ref = "1.0.2")
+devtools::install_github('keboola/r-application', ref = "1.0.2")
+devtools::install_github('keboola/r-docker-application', ref = "1.0.2")
